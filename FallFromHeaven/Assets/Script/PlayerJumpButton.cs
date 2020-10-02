798ef8974c35af9ccb4b338b1fc610cdc6e1b068
johi98 <hgfdsa554@gmail.com>
2020-10-02 18:21:05 +0900

    점프버튼 구현
    Raycast로 점프중인지 확인함
    addForce로 공중으로 날림

---
+++ FallFromHeaven/Assets/Script/PlayerJumpButton.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PlayerJumpButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    [SerializeField] private RectTransform rect_JupmButton = null;
+    [SerializeField] private GameObject go_player = null;
+    [SerializeField] private float jumpPower = 10f;
+    [SerializeField] private float distToGround = 1f;
+    // Start is called before the first frame update
+
+    private Rigidbody rigid;
+    void Start()
+    {
+        rigid = go_player.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if (Physics.Raycast(go_player.transform.position, Vector3.down, distToGround))
+        {
+            rigid.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
+        }
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+
+    }
+
+
+}
