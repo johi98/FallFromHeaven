e6b41b537bc2789584cc72b7f63ed2737fad382c
johi98 <hgfdsa554@gmail.com>
2020-10-18 01:24:02 +0900

    더블 점프 문제 해결
    공중에 있다면 점프를 하지 않도록 변경

--- FallFromHeaven/Assets/Script/PlayerJump.cs
+++ FallFromHeaven/Assets/Script/PlayerJump.cs
@@ -8,6 +8,7 @@ public class PlayerJump : MonoBehaviour
     private Rigidbody rigi;
     public float distToGround = 2;
     public float jumpPower = 2;
+    private bool oneJump = false;
 
 
     // Start is called before the first frame update
@@ -19,15 +20,28 @@ public class PlayerJump : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-     
+        if (Physics.Raycast(gameObject.transform.position, Vector3.down, distToGround)==false)
+        {
+
+            oneJump = false;
+
+        }
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        if(collision.gameObject.tag == "Plateform")
+      
+        if (collision.gameObject.tag == "Plateform"&&oneJump == false)
         {
             rigi.AddForce(Vector3.up * jumpPower * Time.deltaTime, ForceMode.Impulse);
+
+            oneJump = true ;
+                
         }
-       
+      
+
     }
+
+   
+    
 }
