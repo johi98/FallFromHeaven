cb49d3ec27af2e7010cf4ae937e1f307b78d6545
johi98 <hgfdsa554@gmail.com>
2020-10-17 17:33:19 +0900

    점프 및 맵 회전 구현
    현재 버전에서 플레이어가 바닥에 닿을 때에 바닥이 곂쳐있는 경우
    콜라이더 입력을 두번 받아 두배로 점프하게 되는 오류가 있음

---
+++ FallFromHeaven/Assets/Script/CameraMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+
+    public Transform Player;
+
+    public Vector3 Offset;
+
+    public float PositionOffset;
+
+    private void Start()
+    {
+        Vector3 TargetPosition = Player.position + Offset;
+        transform.position = TargetPosition;
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 TargetPosition = Player.position + Offset;
+
+        if(Player.position.y< transform.position.y + PositionOffset)
+        {
+            transform.position = TargetPosition;
+        }
+        
+
+    }
+}
---
+++ FallFromHeaven/Assets/Script/PlayerJump.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerJump : MonoBehaviour
+{
+
+    private Rigidbody rigi;
+    public float distToGround = 2;
+    public float jumpPower = 2;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      rigi =   gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Plateform")
+        {
+            rigi.AddForce(Vector3.up * jumpPower * Time.deltaTime, ForceMode.Impulse);
+        }    
+    }
+}
