e082f8a8c6a5438bd2854de6637de92236d8dc35
johi98 <hgfdsa554@gmail.com>
2020-10-19 22:54:30 +0900

    주석 추가

--- FallFromHeaven/Assets/Script/CreateCylinder.cs
+++ FallFromHeaven/Assets/Script/CreateCylinder.cs
@@ -6,14 +6,17 @@ public class CreateCylinder : MonoBehaviour
 {
     public GameObject cylinder;
     private bool isCreated= false;
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter(Collider other)//콜라이더를 트리거로 설정했기에 트리거로 받아줌
     {
         if (other.gameObject.tag == "Player")
         {
             if(isCreated == false)
             {
                 Instantiate(cylinder, new Vector3(0, cylinder.transform.position.y - 40, 0), Quaternion.identity);
+                //Prefeb으로 저장된 실린더를 다다음의 실린더의 위치에 배치해줌
                 isCreated = true;
+                //만약 배치가 완료되었다면 다시 배치가 안되도록 트루값으로 트리거를 OFF해줌
+                //랜덤으로 실린더를 가져오는 코드 작성예정
             }
             
         }
--- FallFromHeaven/Assets/Script/PlayerJump.cs
+++ FallFromHeaven/Assets/Script/PlayerJump.cs
@@ -14,17 +14,17 @@ public class PlayerJump : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-      rigi =   gameObject.GetComponent<Rigidbody>();
+      rigi =   gameObject.GetComponent<Rigidbody>();//플레이어의 RIGIDBODY를 받아옴
     }
 
     // Update is called once per frame
     void Update()
     {
         if (Physics.Raycast(gameObject.transform.position, Vector3.down, distToGround)==false)
-        {
+        {//스크립트가 들어간 오브젝트의 아래방향으로 distToGround 값 만큼 레이를 발사래 오브젝트가 있으면 true 값을 주는 코드
 
             oneJump = false;
-
+            
         }
     }
 
@@ -32,11 +32,11 @@ public class PlayerJump : MonoBehaviour
     {
       
         if (collision.gameObject.tag == "Plateform"&&oneJump == false)
-        {
+        {   //플레이어가 닿은 오브젝트의 tag가 Plateform이고 점프를 하지 않았다면 
             rigi.AddForce(Vector3.up * jumpPower * Time.deltaTime, ForceMode.Impulse);
-
+            //플레이어에게 수직으로 힘을 주며 deltaTime을 통해 플레이어가 받는 힘을 정규화 시켜줌
             oneJump = true ;
-                
+            //플레이어가 바닥에 있을때만 점프
         }
       
 
