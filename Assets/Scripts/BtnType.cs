a012b3c4eb3ab8ffcdb3d03af1583e9be3abcfc8
jongsun <jongsun_2011@naver.com>
2020-10-20 13:20:11 +0900

    메인 메뉴 1차
    메인 메뉴

---
+++ Assets/Scripts/BtnType.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class BtnType : MonoBehaviour,IPointerEnterHandler,IPointerExitHandler
+{
+    public BTNType currentType;
+    public Transform buttonScale;
+    Vector3 defaultScale;
+
+    private void Start()
+    {
+        defaultScale = buttonScale.localScale;
+    }
+    public void OnBtnClick()
+    {
+        switch (currentType)
+        {
+            case BTNType.Start:
+                Debug.Log("게임 시작");
+                break;
+            case BTNType.Next:
+                Debug.Log("다음으로");
+                break;
+            case BTNType.Back:
+                Debug.Log("전으로");
+                break;
+
+        }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        buttonScale.localScale = defaultScale * 1.2f;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        buttonScale.localScale = defaultScale;
+    }
+}
