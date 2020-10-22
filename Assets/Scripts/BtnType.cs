6640edc38cf123254243df8f3129e839c7209ec6
jongsun <jongsun_2011@naver.com>
2020-10-22 14:10:53 +0900

    메인 메뉴 3차
    1. 모바일 해상도 추가
    2. 터치 기능 구현
    3. 디자인 수정 필요

--- Assets/Scripts/BtnType.cs
+++ Assets/Scripts/BtnType.cs
@@ -2,18 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
 
-public class BtnType : MonoBehaviour,IPointerEnterHandler,IPointerExitHandler
+public class BtnType : MonoBehaviour,IPointerEnterHandler,IPointerExitHandler,IPointerDownHandler
 {
     public BTNType currentType;
     public Transform buttonScale;
     Vector3 defaultScale;
+    public CanvasGroup mainGroup;
+    public CanvasGroup optionGroup;
 
     private void Start()
     {
         defaultScale = buttonScale.localScale;
     }
-    public void OnBtnClick()
+    bool isSound;
+    public void OnPointerDown(PointerEventData eventData) //OnBtnClick()
     {
         switch (currentType)
         {
@@ -24,19 +28,56 @@ public class BtnType : MonoBehaviour,IPointerEnterHandler,IPointerExitHandler
                 Debug.Log("다음으로");
                 break;
             case BTNType.Back:
-                Debug.Log("전으로");
+                CanvasGroupOn(mainGroup);
+                CanvasGroupOff(optionGroup);
+                break;
+            case BTNType.Option:
+                CanvasGroupOn(optionGroup);
+                CanvasGroupOff(mainGroup);
+                break;
+            case BTNType.Sound:
+
+                if (isSound)
+                {
+                    Debug.Log("사운드 OFF");
+                }
+                else
+                {
+                    Debug.Log("사운드 ON");
+                }
+                isSound = !isSound;
+                break;
+            case BTNType.Quit:
+                Application.Quit();
+                Debug.Log("나가기");
                 break;
 
         }
     }
-
-    public void OnPointerEnter(PointerEventData eventData)
+    public void CanvasGroupOn(CanvasGroup cg)
+    {
+        cg.alpha = 1;
+        cg.interactable = true;
+        cg.blocksRaycasts = true;
+    }
+    public void CanvasGroupOff(CanvasGroup cg)
+    {
+        cg.alpha = 0;
+        cg.interactable = false;
+        cg.blocksRaycasts = false;
+    }
+    public void OnPointerEnter(PointerEventData eventData) //버튼 올리면 커지기
     {
         buttonScale.localScale = defaultScale * 1.2f;
     }
 
-    public void OnPointerExit(PointerEventData eventData)
+    public void OnPointerExit(PointerEventData eventData) //버튼 내리면 다시 작아지기
     {
         buttonScale.localScale = defaultScale;
+    }
+
+    public void  GoScene() //씬 이동
+    {
+        SceneManager.LoadScene(1);
     }
 }
