f05aa1f483fb372fce2f209c7085441c9c268e4a
johi98 <hgfdsa554@gmail.com>
2020-11-12 00:44:02 +0900

    맵 제작 및 회전하는 장애물 제작

---
+++ FallFromHeaven/Assets/Script/Spin_Hurdle.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spin_Hurdle : MonoBehaviour
+{
+    float rotate_speed = 50;
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.Rotate(new Vector3(0, rotate_speed , 0)*Time.deltaTime);
+    }
+}
