3cc9c3b17b9eb4542bdc3a01fdefb49261f78086
johi98 <hgfdsa554@gmail.com>
2020-11-25 03:53:08 +0900

    Revert "병합 중"
    This reverts commit b863f9e923414ef30e0014f920778ed9e4f2febc.

--- FallFromHeaven/Assets/Script/Rotator.cs
+++ FallFromHeaven/Assets/Script/Rotator.cs
@@ -8,7 +8,7 @@ public class Rotator : MonoBehaviour
 
     private void FixedUpdate()
     {
-        if(Input.touchCount>0 &&Input.GetTouch(0).phase == TouchPhase.Moved)
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
         {//화면의 닿은 손가락이 0개 초과이고 터치후 움직이고 있을 경우 
             Vector3 Rotation = Input.GetTouch(0).deltaPosition;
             //터치 지점에서 이동한 만큼 값을 Vector3 값으로 받아옴
@@ -16,12 +16,12 @@ public class Rotator : MonoBehaviour
             //사용자가 움직인 화면의  백터 x값을 받아 현재 y값의 기울기를 더하거나 빼서 Rotate로 돌려주는 코드
         }
 
-      /*  if (Input.GetMouseButton(0))
-        {//화면의 닿은 손가락이 0개 초과이고 터치후 움직이고 있을 경우 
-            Vector3 Rotation = new Vector3(0, Input.mousePosition.x, 0);
-            //터치 지점에서 이동한 만큼 값을 Vector3 값으로 받아옴
-            transform.Rotate(Rotation * Time.deltaTime);
-            //사용자가 움직인 화면의  백터 x값을 받아 현재 y값의 기울기를 더하거나 빼서 Rotate로 돌려주는 코드
-        }*/
+        /*  if (Input.GetMouseButton(0))
+          {//화면의 닿은 손가락이 0개 초과이고 터치후 움직이고 있을 경우 
+              Vector3 Rotation = new Vector3(0, Input.mousePosition.x, 0);
+              //터치 지점에서 이동한 만큼 값을 Vector3 값으로 받아옴
+              transform.Rotate(Rotation * Time.deltaTime);
+              //사용자가 움직인 화면의  백터 x값을 받아 현재 y값의 기울기를 더하거나 빼서 Rotate로 돌려주는 코드
+          }*/
     }
 }
