a2ec385063607e83bb7642103714b302f6923a85
johi98 <hgfdsa554@gmail.com>
2020-12-07 18:31:24 +0900

    맵 추가및 생성 로직 수정

--- FallFromHeaven/Assets/Script/CreateCylinder.cs
+++ FallFromHeaven/Assets/Script/CreateCylinder.cs
@@ -8,9 +8,15 @@ public class CreateCylinder : MonoBehaviour
 {
 
     public List<GameObject> cylinderList_L1;
+    public List<GameObject> cylinderList_L2;
+    public List<GameObject> cylinderList_L3;
     private bool isCreated= false;
     int listLength = 0;
     int randomIndex = 0;
+    bool L1_triger = false;
+    bool L2_triger = false;
+    bool L3_triger = false;
+    
 
     private void Awake()
     {
@@ -20,15 +26,50 @@ public class CreateCylinder : MonoBehaviour
     private void OnTriggerEnter(Collider other)//콜라이더를 트리거로 설정했기에 트리거로 받아줌
     {
        
+        if(GameManager.score >= 0 && GameManager.score < 20)
+        {
+            L1_triger = true;
+        }
+        if (GameManager.score >= 20 && GameManager.score < 40)
+        {
+            L1_triger = false;
+            L2_triger = true;
+            
+        }
+        if (GameManager.score >= 40)
+        {
+            L2_triger = false;
+            L3_triger = true;
+        }
 
         if (other.gameObject.tag == "Player")
         {
             if(isCreated == false)
             {
-                listLength = cylinderList_L1.Count;
-                randomIndex = UnityEngine.Random.Range(0, listLength);
+                if (L1_triger)
+                {
+                    listLength = cylinderList_L1.Count;
+                    randomIndex = UnityEngine.Random.Range(0, listLength);
 
-                Instantiate(cylinderList_L1[randomIndex], new Vector3(0, gameObject.transform.position.y - 40, 0), Quaternion.identity);
+                    Instantiate(cylinderList_L1[randomIndex], new Vector3(0, gameObject.transform.position.y - 40, 0), Quaternion.identity);
+
+                }
+                if (L2_triger)
+                {
+                    listLength = cylinderList_L2.Count;
+                    randomIndex = UnityEngine.Random.Range(0, listLength);
+
+                    Instantiate(cylinderList_L2[randomIndex], new Vector3(0, gameObject.transform.position.y - 40, 0), Quaternion.identity);
+
+                }
+                if (L3_triger)
+                {
+                    listLength = cylinderList_L3.Count;
+                    randomIndex = UnityEngine.Random.Range(0, listLength);
+
+                    Instantiate(cylinderList_L3[randomIndex], new Vector3(0, gameObject.transform.position.y - 40, 0), Quaternion.identity);
+
+                }
                 //Prefeb으로 저장된 실린더를 다다음의 실린더의 위치에 배치해줌
                 isCreated = true;
                 //만약 배치가 완료되었다면 다시 배치가 안되도록 트루값으로 트리거를 OFF해줌
--- FallFromHeaven/Assets/Script/Needle_Down.cs
+++ FallFromHeaven/Assets/Script/Needle_Down.cs
@@ -8,6 +8,6 @@ public class Needle_Down : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        gameObject.transform.Translate(new Vector3(0, -5, 0) * Time.deltaTime);
+        gameObject.transform.Translate(new Vector3(0, -3, 0) * Time.deltaTime);
     }
 }
